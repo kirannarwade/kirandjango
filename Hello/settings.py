2d71f9d2b1523533221aaccb9ef2d1dc2f52d69b
kirannarwade <90048456+kirannarwade@users.noreply.github.com>
2021-11-16 14:45:48 +0530

    Update settings.py

--- Hello/settings.py
+++ Hello/settings.py
@@ -148,6 +148,6 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
